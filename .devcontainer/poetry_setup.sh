3a9df7b373d53f573686a42efc359bf0cb749bd8
Mahosaurus <Mahosaurus@users.noreply.github.com>
2023-02-13 14:56:27 +0100

    Adding git bash autocompletion

--- .devcontainer/poetry_setup.sh
+++ .devcontainer/poetry_setup.sh
@@ -1,1 +1,6 @@
 poetry install
+
+# Install Git Autocompletion
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash --insecure
+str="if [ -f ~/.git-completion.bash ] ; then . ~/.git-completion.bash ; fi"
+echo $str >> ~/.bashrc
